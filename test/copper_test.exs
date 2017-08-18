31e9528dfe15577b2980e668200142d327fee7c7
Philip Dexter <philip.dexter@gmail.com>
2017-08-18 16:20:58 +0200

    Add first test case

--- test/copper_test.exs
+++ test/copper_test.exs
@@ -1,8 +1,23 @@
 defmodule CopperTest do
   use ExUnit.Case
-  doctest Copper
+  # doctest Copper
 
-  test "greets the world" do
-    assert Copper.hello() == :world
+  use Copper
+
+  test "mocks a dog" do
+    {Dog, pid} =
+      C.double(Dog)
+      |> CM.give(walk, fn -> "walking!" end)
+      |> CM.give(bark(), "ruff")
+      |> CM.give(count(a), a+1)
+      |> C.build
+
+    assert Dog.walk() == "walking!"
+    assert Dog.bark() == "ruff"
+    assert Dog.count(1) == 2
+    assert GenServer.call(pid, {:callcount, :walk}) == 1
+    assert GenServer.call(pid, {:callcount, :bark}) == 1
+    assert GenServer.call(pid, {:callcount, :count}) == 1
   end
+
 end
