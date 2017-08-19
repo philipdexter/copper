72d38fdfa586251abd3f87bf4b5f5a4322843ffe
Philip Dexter <philip.dexter@gmail.com>
2017-08-19 17:04:50 +0200

    Update yahtzee example

--- examples/yahtzee/lib/yahtzee.ex
+++ examples/yahtzee/lib/yahtzee.ex
@@ -3,9 +3,24 @@ defmodule Yahtzee do
   Documentation for Yahtzee.
   """
 
-  def round() do
-    dice = Dice.roll()
-    Player.decide(dice)
+  def play_round() do
+    play_round([])
+  end
+  defp play_round(saved_dice) do
+    dice = Dice.roll(5-length(saved_dice))
+    case react(saved_dice, dice, Player.decide(saved_dice++dice)) do
+      {:continue, new_saved_dice} -> play_round(new_saved_dice)
+      {:end, match} -> match
+    end
   end
 
+  defp react(saved_dice, rolled_dice, {:reroll, to_reroll}) do
+    all_dice = saved_dice ++ rolled_dice
+    new_saved_dice = Enum.map(Enum.filter(Enum.with_index(all_dice), fn {_die, idx} -> idx not in to_reroll end), fn {die, _idx} -> die end)
+    {:continue, new_saved_dice}
+  end
+  defp react(_saved_dice, _rolled_dice, {:choose, match}) do
+    # todo check if the dice really make the chosen match
+    {:end, match}
+  end
 end
--- examples/yahtzee/test/yahtzee_test.exs
+++ examples/yahtzee/test/yahtzee_test.exs
@@ -6,16 +6,20 @@ defmodule YahtzeeTest do
 
   test "plays one round" do
     C.double(Dice)
-    |> CM.give(roll(), [1,2,3,4,5])
+    |> CM.give_multiple(roll(amt), [[1,2,3,4,1],[2],[5]])
     |> C.build()
 
     C.double(Player)
-    |> CM.give(decide(dice), {:choose, :smallstraight})
+    |> CM.give_multiple(decide(dice), [{:reroll, [4]}, {:reroll, [4]}, {:choose, :smallstraight}])
     |> C.build()
 
-    Yahtzee.round()
+    Yahtzee.play_round()
 
-    CM.verify(Dice.roll())
+    CM.verify(Dice.roll(5))
+    CM.verify(Dice.roll(1))
+    CM.verify(Dice.roll(1))
+    CM.verify(Player.decide([1,2,3,4,1]))
+    CM.verify(Player.decide([1,2,3,4,2]))
     CM.verify(Player.decide([1,2,3,4,5]))
   end
 
