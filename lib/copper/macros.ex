2e5c966a29b115731a6dd97a049bb75a6eb713c9
Philip Dexter <philip.dexter@gmail.com>
2017-08-19 14:16:06 +0200

    Escape args and don't escape func name

--- lib/copper/macros.ex
+++ lib/copper/macros.ex
@@ -3,13 +3,13 @@
 defmodule Copper.Macros do
   defmacro give(double, {func, _, _}, {:fn, _, [{:->, _, [args, body]}]}) do
     quote do
-      Copper.add_handle(unquote(double), unquote(Macro.escape(func)), unquote(Macro.escape(args)), unquote(Macro.escape(body)))
+      Copper.add_handle(unquote(double), unquote(func), unquote(Macro.escape(args)), unquote(Macro.escape(body)))
     end
   end
 
   defmacro give(double, {func, _, args}, return) do
     quote do
-      Copper.add_handle(unquote(double), unquote(func), unquote(args), unquote(return))
+      Copper.add_handle(unquote(double), unquote(func), unquote(Macro.escape(args)), unquote(return))
     end
   end
 
